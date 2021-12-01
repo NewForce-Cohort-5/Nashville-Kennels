be418314a33f139e196cd26ba814ff8a0b5295a3
Tommy Spurlock <tspurloc@gmail.com>
2021-12-01 09:30:01 -0500

    Added customers and locations

---
+++ src/components/customer/CustomerCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Customer.css"
+
+export const CustomerCard = () =>  (
+    <section className="customer">
+        <h3 className="customer__name">Hanna Hall</h3>
+        <div className="customer__adress">100 Infinity Way</div>
+    </section>
+)
